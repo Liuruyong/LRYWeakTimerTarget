cfc8123187c7bc40d282a8ed73136254fc590fb1
liuruyong <466374269@qq.com>
2017-03-28 20:47:10 +0800

    Initial Commit

---
+++ LRYWeakTimerTarget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LRYWeakTimerTarget
+//
+//  Created by mac on 28/03/2017.
+//  Copyright © 2017 liuruyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LRYWeakTimerTarget/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LRYWeakTimerTarget
+//
+//  Created by mac on 28/03/2017.
+//  Copyright © 2017 liuruyong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
